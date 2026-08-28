deccea7317cba372a09ba00e867d75befb4c1251
Tong Lin <tonglinn7@gmail.com>
2026-08-29 00:00:38 +0100

    Update gc_content.py

--- gc_content.py
+++ gc_content.py
@@ -1,18 +1,18 @@
-def GC_content(sequence): # Create a function
+def gc_content(sequence): # Create a function
     """Calculates the GC content of a DNA sequence as a percentage."""
-    total_GCs = 0
+    total_gcs = 0
     # Variable starting at 0 and accumulates
     for nucleotide in sequence.upper():
         # Loops through sequence one character at a time
         # .upper() converts whole sequence to uppercase first
         if nucleotide == "G" or nucleotide == "C":
-            total_GCs += 1
-    return (total_GCs / len(sequence)) * 100
+            total_gcs += 1
+    return (total_gcs / len(sequence)) * 100
 
 if __name__ == "__main__":
     # Code is only run directly, not if imported from this file
     sequence = input("Enter a DNA sequence: ")
-    print(f"GC content: {round(GC_content(sequence), 2)}%")
+    print(f"GC content: {round(gc_content(sequence), 2)}%")
     # f string inserts variable's value inbetween texts
     # Rounded to 2 decimal places
 
